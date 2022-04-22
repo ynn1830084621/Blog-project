a2d1a10cd1cb28573827be840108421eb7a9c0ca
ynn <1830084621@qq.com>
2022-04-22 23:00:05 +0800

    "项目环境搭建"

---
+++ blog/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+function Home() {
+  return (
+    <>
+      <div>
+        <title>Home</title>
+        <Button type="primary">按钮</Button>
+      </div>
+    </>
+  )
+}
+export default Home
