6ffb5059a80bdbb99a8036ddb5510e820025fde2
ynn <1830084621@qq.com>
2022-04-26 23:28:11 +0800

    "中台搭建-egg.js"

---
+++ blog/service/app/controller/home.js
@@ -0,0 +1,16 @@
+// 'use strict';
+
+// const Controller = require('egg').Controller;
+
+// class HomeController extends Controller {
+//   async index() {
+//     const { ctx } = this;
+//     ctx.body = 'hi, egg';
+//   }
+//   async list() {
+//     const { ctx } = this;
+//     ctx.body = '<h2>blog list</h2>';
+//   }
+// }
+
+// module.exports = HomeController;
